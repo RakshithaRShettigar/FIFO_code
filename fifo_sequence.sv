39549a8072c6b42748e245eaefe40b51654d3cc1
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2023-09-11 16:08:48 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -1,5 +1,5 @@
 class fifo_sequence extends uvm_sequence#(fifo_sequence_item);
-  `uvm_object_utils(f_sequence)
+  `uvm_object_utils(fifo_sequence)
   
   function new(string name = "fifo_sequence");
     super.new(name);
@@ -10,21 +10,35 @@ class fifo_sequence extends uvm_sequence#(fifo_sequence_item);
     repeat(1024) begin
       req = fifo_sequence_item::type_id::create("req");
       start_item(req);
-      assert(req.randomize() with {{wr,rd} == 10;});
+      assert(req.randomize() with ({{i_wren,i_rden} == 2'b10;});
       finish_item(req);
     end
     `uvm_info(get_type_name(), $sformatf(""-------------Performing continuous read operation------------"), UVM_LOW)
-    repeat(16) begin
-      req = f_sequence_item::type_id::create("req");
+      repeat(1024) begin
+      req = fifo_sequence_item::type_id::create("req");
       start_item(req);
-      assert(req.randomize() with {{i_wren,i_rden} == 01;});
+        assert(req.randomize() with ({{i_wren,i_rden} == 2'b01;});
       finish_item(req);
     end
-    `uvm_info(get_type_name(), $sformatf("******** Generate 20 Random REQs ********"), UVM_LOW)
-    repeat(20) begin
-      req = f_sequence_item::type_id::create("req");
+   `uvm_info(get_type_name(), $sformatf("-------------Performing random write and read operation------------"), UVM_LOW)
+    repeat(1024) begin
+      req = fifo_sequence_item::type_id::create("req");
       start_item(req);
       assert(req.randomize());
+      finish_item(req);
+    end
+  `uvm_info(get_type_name(), $sformatf("-------------Performing simultaneous write and read operation------------"), UVM_LOW) 
+   repeat(10) begin
+      req = fifo_sequence_item::type_id::create("req");
+      start_item(req);
+     assert(req.randomize()) with ({{i_wren,i_rden} == 2'b11;});
+      finish_item(req);
+    end
+             `uvm_info(get_type_name(), $sformatf("-------------Performing no write and no read operation------------"), UVM_LOW) 
+   repeat(10) begin
+      req = fifo_sequence_item::type_id::create("req");
+      start_item(req);
+     assert(req.randomize()) with ({{i_wren,i_rden} == 2'b00;});
       finish_item(req);
     end
   endtask
